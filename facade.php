b017a5cd82020941361a77a0400af1cf1beec13f
KingGimer44 <150565033+KingGimer44@users.noreply.github.com>
2024-11-29 11:02:07 -0600

    Update facade.php

--- facade.php
+++ facade.php
@@ -1,22 +1,47 @@
 <?php
-
-require_once 'NegocioMedicamento.php';
+require_once 'MedicamentoLee.php';
+require_once 'MedicamentoEscribe.php';
 require_once 'Singleton.php';
 require_once 'sujeto.php';
 require_once 'observer.php';
 
 class InventarioFacade extends Sujeto {
-    private $medicamentoNegocio;
+    private $medicamentoQueryService;
+    private $medicamentoCommandService;
 
     public function __construct() {
-        $conexion = Singleton::getInstance()->getConexion();
-        $this->medicamentoNegocio = new Medicamento($conexion);
+        $this->medicamentoQueryService = new MedicamentoQueryService();
+        $this->medicamentoCommandService = new MedicamentoCommandService();
     }
 
     public function obtenerTodosLosMedicamentos() {
-        $medicamentos = $this->medicamentoNegocio->obtenerTodos();
+        $medicamentos = $this->medicamentoQueryService->obtenerTodos();
         $this->notificar("Se han obtenido " . count($medicamentos) . " medicamentos.");
         return $medicamentos;
+    }
+
+    public function agregarMedicamento($nombre, $descripcion, $cantidad, $imagen) {
+        $resultado = $this->medicamentoCommandService->agregarMedicamento($nombre, $descripcion, $cantidad, $imagen);
+        if ($resultado) {
+            $this->notificar("Se ha agregado un nuevo medicamento: " . $nombre);
+        }
+        return $resultado;
+    }
+
+    public function modificarMedicamento($id, $nombre, $descripcion, $cantidad, $imagen) {
+        $resultado = $this->medicamentoCommandService->modificarMedicamento($id, $nombre, $descripcion, $cantidad, $imagen);
+        if ($resultado) {
+            $this->notificar("Se ha actualizado el medicamento con ID: " . $id);
+        }
+        return $resultado;
+    }
+
+    public function eliminarMedicamento($id) {
+        $resultado = $this->medicamentoCommandService->eliminarMedicamento($id);
+        if ($resultado) {
+            $this->notificar("Se ha eliminado el medicamento con ID: " . $id);
+        }
+        return $resultado;
     }
 
 }
